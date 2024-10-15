8b8f21b9ec4a097847a3be307708719a02b7bdae
rohitjagtap731 <rohitjagtap731@gmail.com>
2024-10-15 10:50:08 +0530

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Hero.css';
+import profile_img from '../../assets/profile_img.svg'
+
+const Hero = () =>{
+    return (
+        <div className="hero">
+            <img src={profile_img} alt=""/>
+            <h1><span>I'm Rohit Jagtap,</span> Software Devloper based in India</h1>
+            <div className="hero-action">
+                <div className="hero-connect">Connect with me</div>
+                <div className="hero-resume">My resume</div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
