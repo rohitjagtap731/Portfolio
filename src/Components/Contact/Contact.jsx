f78229965c8a13b420be59ba631536a5c63e933c
rohitjagtap731 <rohitjagtap731@gmail.com>
2024-10-19 00:50:26 +0530

    responsive + web3form + anchrlink

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,6 +6,29 @@ import location_icon from '../../assets/theme_pattern.svg';
 import call_icon from '../../assets/call_icon.svg';
 
 const Contact = () => {
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "f2427d9c-3d33-4ff6-beae-39fa35f7a2a7");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          alert(res.message);
+        }
+      };
+
     return (
         <div id="contact" className="contact">
             <div className="contact-title">
@@ -28,7 +51,7 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
-                <form className="contact-right">
+                <form onSubmit={onSubmit} className="contact-right">
                     <label htmlFor="">Your Name</label>
                     <input type="text" placeholder="Enter your name" name='name' />
                     <label htmlFor="">Your Email</label>
