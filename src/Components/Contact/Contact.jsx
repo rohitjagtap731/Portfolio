d2f76875a5000cc64b1ef82a1f640bb24c696a69
rohitjagtap731 <rohitjagtap731@gmail.com>
2024-10-20 01:05:26 +0530

    make more responsive

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './Contact.css';
 import theme_pattern from '../../assets/theme_pattern.svg';
 import mail_icon from '../../assets/mail_icon.svg';
-import location_icon from '../../assets/theme_pattern.svg';
+import location_icon from '../../assets/location_icon.svg';
 import call_icon from '../../assets/call_icon.svg';
 
 const Contact = () => {
