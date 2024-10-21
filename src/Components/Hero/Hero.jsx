7d50f1b7311b35ac566d136df16ee37386125155
rohitjagtap731 <rohitjagtap731@gmail.com>
2024-10-21 10:25:32 +0530

    Make more responsive using about

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () =>{
     return (
         <div id="home" className="hero">
             <img src={profile_img} alt=""/>
-            <h1><span>I'm Rohit Jagtap,</span> Software Devloper based in India</h1>
+            <h1><span>I'm Rohit Jagtap,</span> Software Devloper based in India.</h1>
             <p>I am a software developer from Pune, India with 2 years of experience in Aloha Technology pvt.</p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
